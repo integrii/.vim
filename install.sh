8e5cf7d255628aca40306aa389e42cdc28c60ab8
Eric Greer <integrii@gmail.com>
2015-07-09 17:55:11 -0700

    adding clone to install script

--- install.sh
+++ install.sh
@@ -11,13 +11,14 @@ if [[ -d ~/.vim ]]; then
 	cp -r ~/.vim ~/.vim.old
 	rm -rf ~/.vim
 	echo "Moved ~/.vim to ~/.vim.old"
+	git clone git@github.com:integrii/.vim.git ~/.vim
 fi
 
 
 # install .vimrc symlink
 if [[ -f ~/.vimrc ]]; then
 	cp ~/.vimrc ~/.vimrc.old
-	rm -f ~/.vimrc
+	rm -rf ~/.vimrc
 	echo "Moved ~/.vimrc to ~/.vimrc.old"
 fi
 ln -s ~/.vim/.vimrc ~/.vimrc
